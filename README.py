d2a9e4b76d34a9864b69909cb394ab2024ca1e0e
Samuel <dsh20462046@gmail.com>
2017-06-02 14:30:42 -0400

    Update README.py

--- README.py
+++ README.py
@@ -6,8 +6,8 @@ from botocore.client import Config
 
 s3 = boto3.resource(
     's3',
-    aws_access_key_id='AKIAIWOJJA2TRONDEPWQ',
-    aws_secret_access_key='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',
+    aws_access_key_id='AKIAIWOJJA2TRONDWEDDDEWEEE',
+    aws_secret_access_key='bprLAte5uFgzxAduy5lQTCcbXQADHq5xR9S23k0AQAWW',
     config=Config(signature_version='s3v4')
 )
 
